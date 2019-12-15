ad48bbeb71489fe0ed812c8c3b6ae7e733e59a2a
thomas-kunnumpurath <madmaludawg@gmail.com>
2019-12-14 23:12:37 -0500

    Update join.ts

--- battleship_frontend/src/player-app/join.ts
+++ battleship_frontend/src/player-app/join.ts
@@ -10,6 +10,7 @@ import { SolaceClient } from "../common/solace-client";
 @inject(Router, SolaceClient, Player, TopicHelper, GameStart)
 export class Join {
   pageState = "PLAYER_DETAILS"; // PLAYER_DETAILS => WAITING
+  pageStatus = "Waiting for other player to join...";
   playerNickname: string = null;
 
   constructor(private router: Router, private solaceClient: SolaceClient, private player: Player, private topicHelper: TopicHelper, private gameStart: GameStart) {}
@@ -47,12 +48,19 @@ export class Join {
     let playerJoined: PlayerJoined = new PlayerJoined();
     playerJoined.playerName = this.player.name;
     playerJoined.playerNickname = this.playerNickname;
-
-    //Publish a join event and change the pageState to waiting
-    let topicName: string = `${this.topicHelper.prefix}/JOIN/${this.player.name.toUpperCase()}`;
-    this.solaceClient.publish(topicName, JSON.stringify(playerJoined));
-
-    this.pageState = "WAITING";
+    //Publish a join request and change the pageState to waiting if the join request succeeded
+    let topicName: string = `${this.topicHelper.prefix}/JOIN-REQUEST/${this.player.getPlayerNameForTopic()}`;
+    let replyTopic: string = `${this.topicHelper.prefix}/JOIN-REPLY/${this.player.getOtherPlayerNameForTopic()}`;
+    this.solaceClient
+      .sendRequest(topicName, JSON.stringify(playerJoined), replyTopic)
+      .then((msg: any) => {
+        let joinResult: JoinResult = JSON.parse(msg.getBinaryAttachment());
+        if (joinResult.success) this.pageState = "WAITING";
+        else this.pageStatus = "Join Request Failed - Player Already Joined!";
+      })
+      .catch(error => {
+        this.pageStatus = "Join Request Failed!";
+      });
   }
 
   detached() {}
