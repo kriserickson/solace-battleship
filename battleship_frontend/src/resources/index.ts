dbcea4f9a5f498bb9984617eb0346d8c99ffae77
thomas-kunnumpurath <madmaludawg@gmail.com>
2019-12-29 12:56:02 -0600

    putting codelab in master

--- battleship_frontend/src/resources/index.ts
+++
@@ -1,7 +0,0 @@
-import {FrameworkConfiguration, PLATFORM} from 'aurelia-framework';
-
-
-export function configure(config: FrameworkConfiguration) {
-  //Load the SolaceClient connection library on startup
-  config.globalResources([PLATFORM.moduleName('../common/solace-client')]);
-}
